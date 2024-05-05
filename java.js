8a446696653540969f7221afddc5130eda48c956
Thu Vân <162014956+ThuVan107@users.noreply.github.com>
2024-05-05 15:05:59 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,49 @@
+var images = document.querySelectorAll('.image img')
+var prev = document.querySelector('.prev')
+var next = document.querySelector('.next')
+var close = document.querySelector('.close')
+var galleryImg = document.querySelector('.gallery_inner img')
+var galery = document.querySelector('.gallery')
+var currentIndex = 0;
+function showGallery() {
+if (currentIndex == 0) {
+prev.classList.add('hide')
+}
+else{
+prev.classList.remove('hide')
+}
+if (currentIndex == image.length -1) {
+prev.classList.add('hide')
+}
+else{
+prev.classList.remove('hide')
+}
+}
+
+image.forEach((item, index)=>{
+    item.addEventListenner ('click', function() {
+    currentIndex = index
+    showGallery()
+    })
+    })
+    close.addEventListener('click', function () {
+        gallery.classList.remove('show')
+    })
+    document.addEventListener('keydown', function () {
+    if (e.keyCode ==27) {
+        gallery.classList.remove('show')
+    }
+    })
+
+    prev.addEventListener('click', function () {
+        if (currentIndex > 0) {
+        currentIndex--
+        showGallery()
+        }
+      })
+        next.addEventListener('click', function () {
+        if (currentIndex < images.length -1) {
+        currentIndex++
+        showGallery()
+        }
+        })
